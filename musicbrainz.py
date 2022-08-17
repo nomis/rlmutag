9eacb101259e4f76e17b8bf04eab98551542ff3f
Simon Arlott <sa.me.uk>
2022-08-17 19:59:28 +0100

    Always output titles/artists to two files

--- musicbrainz.py
+++ musicbrainz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
-import urllib.request
 import sys
+import urllib.request
 
 
 def fix(text):
@@ -16,9 +16,9 @@ def fix(text):
 with urllib.request.urlopen("https://musicbrainz.org/ws/2/release/" + sys.argv[1] + "?inc=artist-credits+recordings&fmt=json") as f:
 	x = json.loads(f.read())
 
-for media in x["media"]:
-	for track in media["tracks"]:
-		if sys.argv[2] == "artist":
-			print(fix("".join([artist["name"] + artist["joinphrase"] for artist in track["artist-credit"]])))
-		elif sys.argv[2] == "title":
-			print(fix(track["title"]))
+with open("a", "wt") as a:
+	with open("t", "wt") as t:
+		for media in x["media"]:
+			for track in media["tracks"]:
+				print(fix("".join([artist["name"] + artist["joinphrase"] for artist in track["artist-credit"]])), file=a)
+				print(fix(track["title"]), file=t)
